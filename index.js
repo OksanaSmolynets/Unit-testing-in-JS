9592036e1c561c6be5e6927256139dfb926c3d1e
Oksana Smolynets <oksana.mavdryk25@gmail.com>
2022-03-29 02:51:28 +0300

    unit tests for functions

---
+++ index.js
@@ -0,0 +1,49 @@
+// Task 86(в)
+// Дано натуральне число n. Вивести першу його цифру
+let firstNumber = n => {
+    if (typeof n === 'number') {
+        let absNumber = Math.abs(n);
+        let firstNumber = Number((Array.from(absNumber.toString())[0]));
+        console.log(firstNumber);
+        return firstNumber;
+    } else {
+        console.log('Parameter is not number!');
+    }
+}
+
+// Task 563 (б)
+// Знайти всі менші 100 числа-паліндроми, які при піднесенні до квадрату є також паліндромами
+
+// A function that takes a number and returns it if the number is a palindrome
+let isPalindromNumber = (num) => {
+    let absNumber = Math.abs(num),
+        arrOfNumber = Array.from(absNumber.toString()),
+        arrOfNumberReverce = Array.from(absNumber.toString()).reverse(),
+        isPalindrom = (JSON.stringify(arrOfNumber) === JSON.stringify(arrOfNumberReverce)) ? true : false;
+
+    if (isPalindrom) {
+        return num
+    } else return 0
+}
+
+// A function, which looking for palindromes from squares of palindromes
+let palindromOfNumSquare = () => {
+    // create an array from 1 to 100
+    let arr = [];
+    for (let i = 0; i < 99; i++) {
+        arr[i] = i + 1;
+    }
+
+    // choose from an array of numbers palindromes and raise them to the square
+    let palindromToPower = arr.filter(item => isPalindromNumber(item))
+        .map(item => Math.pow(isPalindromNumber(item), 2));
+
+    // choose from the squares of palindromes palindromes
+    let palindronNumInPower = palindromToPower.filter(item => isPalindromNumber(item))
+    console.log(palindronNumInPower);
+    return palindronNumInPower;
+}
+
+palindromOfNumSquare();
+
+module.exports = { firstNumber, isPalindromNumber, palindromOfNumSquare }
---
+++ index.test.js
@@ -0,0 +1,60 @@
+const { firstNumber, isPalindromNumber, palindromOfNumSquare } = require('./index.js');
+
+describe('firstNumber function', () => {
+    test('first number is equal', () => {
+        expect(firstNumber(24)).toBe(2);
+        expect(firstNumber(100)).toBe(1);
+        expect(firstNumber(-3400)).toBe(3);
+        expect(firstNumber(98)).toEqual(9);
+    });
+    test('first number logic', () => {
+        expect(firstNumber(24)).toBeDefined();
+        expect(firstNumber(9)).toBeGreaterThanOrEqual(0);
+        expect(firstNumber(0.5)).not.toBeLessThan(0);
+    });
+})
+
+describe('isPalindromNumber function', () => {
+    test('isPalindromNumber is equal', () => {
+        expect(isPalindromNumber(99)).toBe(99);
+        expect(isPalindromNumber(44)).toBe(44);
+        expect(isPalindromNumber(22)).toBe(22);
+        expect(isPalindromNumber(24)).toBe(0);
+        expect(isPalindromNumber(100)).toBe(0);
+        expect(isPalindromNumber(78)).toBe(0);
+        expect(isPalindromNumber(78)).not.toBe(78);
+    });
+    test('isPalindromNumber logic', () => {
+        expect(isPalindromNumber(100)).toBeDefined();
+        expect(isPalindromNumber(75)).toBeFalsy();
+        expect(isPalindromNumber(0)).toBeDefined();
+        expect(isPalindromNumber(1)).toBeDefined();
+        expect(isPalindromNumber(99)).not.toBeFalsy();
+        expect(isPalindromNumber(77)).not.toBeFalsy();
+        expect(isPalindromNumber(77)).not.toBeNull();
+        expect(isPalindromNumber(45)).not.toBeNull();
+    });
+})
+
+describe('palindromOfNumSquare function', () => {
+    test('palindromOfNumSquare is equal', () => {
+        expect(palindromOfNumSquare()).toContain(1);
+        expect(palindromOfNumSquare()).toContain(121);
+        expect(palindromOfNumSquare()).toContain(484);
+        expect(palindromOfNumSquare()).toContain(4);
+        expect(palindromOfNumSquare()[0]).toBe(1);
+        expect(palindromOfNumSquare()[1]).toBeGreaterThan(1);
+        expect(palindromOfNumSquare()).toEqual(expect.arrayContaining([1, 4, 9, 121, 484]));
+    });
+    test('palindromOfNumSquare logic', () => {
+        expect(palindromOfNumSquare().some((item => item === 1))).toBe(true);
+        expect(palindromOfNumSquare().some((item => item !== 1))).toBe(true);
+        expect(palindromOfNumSquare().every((item => item === 1))).toBe(false);
+        expect(palindromOfNumSquare().every((item => item === 0))).toBe(false);
+        expect(palindromOfNumSquare()).toEqual(expect.not.arrayContaining([0]))
+        expect(palindromOfNumSquare().length).toBe(5);
+        expect(palindromOfNumSquare()).toBeDefined();
+        expect(palindromOfNumSquare()).not.toBeFalsy();
+        expect(palindromOfNumSquare()).not.toBeNull();
+    });
+})
